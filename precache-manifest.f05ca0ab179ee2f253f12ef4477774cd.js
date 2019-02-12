b225a12a3a1bd026e8d4016593d8033d8be5cdc7
Muaykhao86 <waveflow18@gmail.com>
2019-02-12 10:28:36 -0800

    Updates

---
+++ precache-manifest.f05ca0ab179ee2f253f12ef4477774cd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "aedb4e53e89ac656e8a6",
+    "url": "/robofriends/static/js/runtime~main.aedb4e53.js"
+  },
+  {
+    "revision": "a49d294bf575bcc7d560",
+    "url": "/robofriends/static/js/main.a49d294b.chunk.js"
+  },
+  {
+    "revision": "c2e1c880a545b21fe2a3",
+    "url": "/robofriends/static/js/2.c2e1c880.chunk.js"
+  },
+  {
+    "revision": "a49d294bf575bcc7d560",
+    "url": "/robofriends/static/css/main.38717a61.chunk.css"
+  },
+  {
+    "revision": "c2e1c880a545b21fe2a3",
+    "url": "/robofriends/static/css/2.a254e910.chunk.css"
+  },
+  {
+    "revision": "3c33b65a871e1fe7f62b1a97edf6692d",
+    "url": "/robofriends/index.html"
+  }
+];
