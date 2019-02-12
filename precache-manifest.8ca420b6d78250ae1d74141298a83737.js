e22f3e3c0d9052ca0362285277736e802138fdc1
Muaykhao86 <waveflow18@gmail.com>
2019-02-12 11:47:35 -0800

    Updates

--- precache-manifest.8ca420b6d78250ae1d74141298a83737.js
+++ precache-manifest.8ca420b6d78250ae1d74141298a83737.js
@@ -8,23 +8,23 @@ self.__precacheManifest = [
     "url": "/robofriends/static/js/runtime~main.aedb4e53.js"
   },
   {
-    "revision": "941478dad3f8024e2391",
-    "url": "/robofriends/static/js/main.941478da.chunk.js"
+    "revision": "360206a43fbf5cf6b14b",
+    "url": "/robofriends/static/js/main.360206a4.chunk.js"
   },
   {
     "revision": "c2e1c880a545b21fe2a3",
     "url": "/robofriends/static/js/2.c2e1c880.chunk.js"
   },
   {
-    "revision": "941478dad3f8024e2391",
-    "url": "/robofriends/static/css/main.6f66ab20.chunk.css"
+    "revision": "360206a43fbf5cf6b14b",
+    "url": "/robofriends/static/css/main.3e46eb47.chunk.css"
   },
   {
     "revision": "c2e1c880a545b21fe2a3",
     "url": "/robofriends/static/css/2.a254e910.chunk.css"
   },
   {
-    "revision": "74328fc2f811a75033a426bca89a834c",
+    "revision": "2e0c6897e2afd9ad2d6bf0e6e5ea970f",
     "url": "/robofriends/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/robofriends/precache-manifest.4ff67aa8d5529a3dbeb43705a3029068.js"
+  "/robofriends/precache-manifest.8ca420b6d78250ae1d74141298a83737.js"
 );
 
 workbox.clientsClaim();
