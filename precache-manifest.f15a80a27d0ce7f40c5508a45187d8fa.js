6946b443e6bf35eb076ae2169b6549276d369158
Muaykhao86 <waveflow18@gmail.com>
2019-04-02 14:11:49 +0100

    Updates

--- precache-manifest.f15a80a27d0ce7f40c5508a45187d8fa.js
+++ precache-manifest.f15a80a27d0ce7f40c5508a45187d8fa.js
@@ -12,23 +12,23 @@ self.__precacheManifest = [
     "url": "/robofriends/static/js/runtime~main.fb41d28d.js"
   },
   {
-    "revision": "0ace7406783a8648859a",
-    "url": "/robofriends/static/js/main.89cc1514.chunk.js"
+    "revision": "4297511c3f67d2ece6e6",
+    "url": "/robofriends/static/js/main.a422770e.chunk.js"
   },
   {
     "revision": "738798d70d976e75127f",
     "url": "/robofriends/static/js/2.d37d5a1c.chunk.js"
   },
   {
-    "revision": "0ace7406783a8648859a",
-    "url": "/robofriends/static/css/main.14776956.chunk.css"
+    "revision": "4297511c3f67d2ece6e6",
+    "url": "/robofriends/static/css/main.8a451f71.chunk.css"
   },
   {
     "revision": "738798d70d976e75127f",
     "url": "/robofriends/static/css/2.6af3676c.chunk.css"
   },
   {
-    "revision": "ec3e1f4ac2aef77f136c131e13b8aeac",
+    "revision": "5d7f73994ee00a22a2643510f1334d41",
     "url": "/robofriends/index.html"
   }
 ];
